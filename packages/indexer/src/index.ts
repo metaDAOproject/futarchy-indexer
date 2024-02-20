36820ddefd002186370b166fb12a054d294c37ec
decartes.sol <123273990+decartesol@users.noreply.github.com>
2024-02-20 05:40:36 +0000

    add python and glibc to dockerfile to get bun install working

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -2,3 +2,7 @@ import { connection } from './connection';
 
 const latestBlockHash = await connection.getLatestBlockhash();
 console.log(latestBlockHash);
+
+setInterval(() => {
+    console.log(new Date().toISOString());
+}, 5000);
