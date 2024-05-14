1f28aaeb57a11c7246409c910185a3ae8ba966fb
decartes.sol <123273990+decartesol@users.noreply.github.com>
2024-05-14 09:10:59 -0700

    Indexer logic (#38)
    * indexer logic
    
    * adding indexing background jobs

--- packages/indexer/src/indexers/index.ts
+++ packages/indexer/src/indexers/index.ts
@@ -1,5 +1,37 @@
 
+export enum IndexerImplementation {
+
+}
+
+class Indexer {
+  private name: string;
+  private implementation: IndexerImplementation;
+  private latestSlotProcessed: BigInt;
+  private dependencies: string[];
+  
+  public constructor(
+    name: string,
+    implementation: IndexerImplementation,
+    latestSlotPorcessed: BigInt,
+    dependencies: string[]) {
+    this.name = name;
+    this.implementation = implementation;
+    this.latestSlotProcessed = latestSlotPorcessed;
+    this.dependencies = dependencies;
+  }
+
+  public stop() {
+
+  }
+  
+  public start() {
+    
+  }
+}
+
 
 export async function startIndexers() {
-  
+  // Get indexers
+
+  // Create indexers that are new, shut down those that are old
 }
