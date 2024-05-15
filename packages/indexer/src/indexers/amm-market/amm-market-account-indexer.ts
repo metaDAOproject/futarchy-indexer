873bcc5b05ba478f2046c2aba3f4e6ea8d818d10
LukasDeco <ldeconantsesznak@gmail.com>
2024-05-15 15:28:49 +0800

    feat: interval indexing twap and conditiona $

--- packages/indexer/src/indexers/amm-market/amm-market-account-indexer.ts
+++ packages/indexer/src/indexers/amm-market/amm-market-account-indexer.ts
@@ -1,16 +1,7 @@
 import { AccountInfoIndexer } from "../account-info-indexer";
-import { provider, rpcReadClient } from "../../connection";
 import { AccountInfo, Context, PublicKey } from "@solana/web3.js";
-import { schema, usingDb } from "@metadaoproject/indexer-db";
 import { Err, Ok } from "../../match";
-import { BN } from "@coral-xyz/anchor";
-import { PricesType } from "@metadaoproject/indexer-db/lib/schema";
-import { enrichTokenMetadata } from "@metadaoproject/futarchy-sdk";
-import { PriceMath } from "@metadaoproject/futarchy-ts";
-import {
-  TwapRecord,
-  PricesRecord,
-} from "@metadaoproject/indexer-db/lib/schema";
+import { indexAmmMarketAccountWithContext } from "./utils";
 
 export enum AmmAccountIndexerError {
   GeneralError = "GeneralError",
@@ -23,90 +14,17 @@ export const AmmMarketAccountUpdateIndexer: AccountInfoIndexer = {
     context: Context
   ) => {
     try {
-      const ammMarketAccount = await rpcReadClient.markets.amm.decodeMarket(
-        accountInfo
-      );
-      const baseToken = await enrichTokenMetadata(
-        ammMarketAccount.baseMint,
-        provider
-      );
-      const quoteToken = await enrichTokenMetadata(
-        ammMarketAccount.quoteMint,
-        provider
+      const res = await indexAmmMarketAccountWithContext(
+        accountInfo,
+        account,
+        context
       );
 
-      // agg is 0 so skipping
-      if (ammMarketAccount.oracle.aggregator.toNumber() === 0)
-        return Ok({ acct: account.toString() });
-
-      // indexing the twap
-      const twapCalculation: BN = ammMarketAccount.oracle.aggregator.div(
-        ammMarketAccount.oracle.lastUpdatedSlot.sub(
-          ammMarketAccount.createdAtSlot
-        )
-      );
-      const twapNumber: number = twapCalculation.toNumber();
-      const newTwap: TwapRecord = {
-        curTwap: BigInt(twapNumber),
-        marketAcct: account.toString(),
-        observationAgg: ammMarketAccount.oracle.aggregator
-          .toNumber()
-          .toString(),
-        proposalAcct: ammMarketAccount.proposal.toString(),
-        // alternatively, we could pass in the context of the update here
-        updatedSlot: context
-          ? BigInt(context.slot)
-          : BigInt(ammMarketAccount.oracle.lastUpdatedSlot.toNumber()),
-      };
-
-      // TODO batch commits across inserts - maybe with event queue
-      const twapUpsertResult = await usingDb((db) =>
-        db
-          .insert(schema.twaps)
-          .values(newTwap)
-          .onConflictDoUpdate({
-            target: [schema.twaps.updatedSlot, schema.twaps.marketAcct],
-            set: newTwap,
-          })
-          .returning({ marketAcct: schema.twaps.marketAcct })
-      );
-
-      if (twapUpsertResult.length === 0) {
-        console.error("failed to upsert twap");
+      if (res.success) {
+        console.log(res.ok);
+        return Ok({ acct: account.toBase58() });
       }
-
-      // indexing the conditional market price
-      const conditionalMarketSpotPrice = PriceMath.getHumanPrice(
-        PriceMath.getAmmPriceFromReserves(
-          ammMarketAccount?.baseAmount,
-          ammMarketAccount?.quoteAmount
-        ),
-        baseToken.decimals!!,
-        quoteToken.decimals!!
-      );
-      const newAmmConditionaPrice: PricesRecord = {
-        marketAcct: account.toString(),
-        updatedSlot: context
-          ? BigInt(context.slot)
-          : BigInt(ammMarketAccount.oracle.lastUpdatedSlot.toNumber()),
-        price: conditionalMarketSpotPrice.toString(),
-        pricesType: PricesType.Conditional,
-        baseAmount: BigInt(ammMarketAccount.baseAmount.toNumber()),
-        quoteAmount: BigInt(ammMarketAccount.quoteAmount.toNumber()),
-      };
-
-      const pricesInsertResult = await usingDb((db) =>
-        db
-          .insert(schema.prices)
-          .values(newAmmConditionaPrice)
-          .onConflictDoUpdate({
-            target: [schema.prices.createdAt, schema.prices.marketAcct],
-            set: newAmmConditionaPrice,
-          })
-          .returning({ marketAcct: schema.prices.marketAcct })
-      );
-
-      return Ok({ acct: pricesInsertResult[0].marketAcct });
+      return res;
     } catch (e) {
       console.error("general error with indexing amm market account info:", e);
       return Err({ type: AmmAccountIndexerError.GeneralError });
---
+++ packages/indexer/src/indexers/amm-market/amm-market-account-interval-indexer.ts
@@ -0,0 +1,46 @@
+import { PublicKey } from "@solana/web3.js";
+import { Err, Ok } from "../../match";
+import { indexAmmMarketAccountWithContext } from "./utils";
+import { IntervalFetchIndexer } from "../interval-fetch-indexer";
+import { connection } from "../../connection";
+
+export enum AmmAccountIntervalIndexerError {
+  General = "General",
+  InvalidRPCResponse = "InvalidRPCResponse",
+  BlankAccountAddr = "BlankAccountAddr",
+}
+
+export const AmmMarketAccountIntervalFetchIndexer: IntervalFetchIndexer = {
+  intervalMs: 60_000,
+  index: async (acct: string) => {
+    if (acct === "") {
+      return Err({
+        type: AmmAccountIntervalIndexerError.BlankAccountAddr,
+      });
+    }
+    try {
+      const account = new PublicKey(acct);
+      const resWithContext = await connection.getAccountInfoAndContext(account);
+      if (!resWithContext.value) {
+        return Err({
+          type: AmmAccountIntervalIndexerError.InvalidRPCResponse,
+        });
+      }
+
+      const res = await indexAmmMarketAccountWithContext(
+        resWithContext.value,
+        account,
+        resWithContext.context
+      );
+
+      if (res.success) {
+        console.log(res.ok);
+        return Ok({ acct: account.toBase58() });
+      }
+      return res;
+    } catch (e) {
+      console.error("general error with indexing amm market account info:", e);
+      return Err({ type: AmmAccountIntervalIndexerError.General });
+    }
+  },
+};
