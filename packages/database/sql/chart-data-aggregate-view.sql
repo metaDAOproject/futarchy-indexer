156429044b2d83806dc32e5dbb91444e859224d8
Kollan House <kollanh@gmail.com>
2024-05-21 14:42:24 +0800

    feat: improved query without need for type

--- packages/database/sql/chart-data-aggregate-view.sql
+++ packages/database/sql/chart-data-aggregate-view.sql
@@ -19,47 +19,32 @@ CREATE OR REPLACE VIEW public."prices_chart_data" AS
 
 
 CREATE OR REPLACE VIEW public."proposal_prices_chart_data" AS
-  WITH proposal_markets AS
-  (
-    SELECT proposal_acct,
-          pass_market_acct,
-          fail_market_acct
-    FROM proposals
-  ),
-  pass_market_bucket AS
+  WITH pass_market_bucket AS
   (
     SELECT proposal_acct,
           TIME_BUCKET('10 SECONDS'::INTERVAL, prices.created_at) AS interv,
-          last(price, prices.created_at) FILTER(WHERE prices.created_at IS NOT NULL) AS price,
-          last(base_amount, prices.created_at) FILTER(WHERE prices.created_at IS NOT NULL) AS base_amount,
-          last(quote_amount, prices.created_at) FILTER(WHERE prices.created_at IS NOT NULL) AS quote_amount,
-          prices_type,
-          market_acct
+          last(prices, prices.created_at) FILTER(WHERE prices.created_at IS NOT NULL) AS prices_row
     FROM proposals
     JOIN prices
     ON proposals.pass_market_acct = prices.market_acct
-    GROUP BY proposal_acct, interv, prices_type, market_acct
+    GROUP BY proposal_acct, interv
   ),
   fail_market_bucket AS
   (
     SELECT proposal_acct,
           TIME_BUCKET('10 SECONDS'::INTERVAL, prices.created_at) AS interv,
-          last(price, prices.created_at) FILTER(WHERE prices.created_at IS NOT NULL) AS price,
-          last(base_amount, prices.created_at) FILTER(WHERE prices.created_at IS NOT NULL) AS base_amount,
-          last(quote_amount, prices.created_at) FILTER(WHERE prices.created_at IS NOT NULL) AS quote_amount,
-          prices_type,
-          market_acct
+          last(prices, prices.created_at) FILTER(WHERE prices.created_at IS NOT NULL) AS prices_row
     FROM proposals
     JOIN prices
     ON proposals.fail_market_acct = prices.market_acct
-    GROUP BY proposal_acct, interv, prices_type, market_acct
+    GROUP BY proposal_acct, interv
   ),
   grouped AS
   (
     SELECT proposal_acct,
           interv,
-          CASE WHEN fmb.price IS NULL THEN NULL ELSE ROW(fmb.price, fmb.base_amount, fmb.quote_amount, fmb.prices_type, fmb.market_acct)::price_ffil_record END AS fmb_values,
-          CASE WHEN pmb.price IS NULL THEN NULL ELSE ROW(pmb.price, pmb.base_amount, pmb.quote_amount, pmb.prices_type, pmb.market_acct)::price_ffil_record END AS pmb_values
+          fmb.prices_row AS fmb_values,
+          pmb.prices_row AS pmb_values
     FROM fail_market_bucket fmb
     FULL OUTER JOIN pass_market_bucket pmb
     USING (proposal_acct, interv)
@@ -68,22 +53,22 @@ CREATE OR REPLACE VIEW public."proposal_prices_chart_data" AS
   (
     SELECT proposal_acct,
           interv,
-          (array_agg(fmb_values) FILTER (WHERE fmb_values IS NOT NULL) OVER (PARTITION BY proposal_acct ORDER BY interv DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING))[1] AS fmb_value,
-          (array_agg(pmb_values) FILTER (WHERE pmb_values IS NOT NULL) OVER (PARTITION BY proposal_acct ORDER BY interv DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING))[1] AS pmb_value
+          (array_agg(fmb_values) FILTER (WHERE (fmb_values).price IS NOT NULL) OVER (PARTITION BY proposal_acct ORDER BY interv DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING))[1] AS fmb_value,
+          (array_agg(pmb_values) FILTER (WHERE (pmb_values).price IS NOT NULL) OVER (PARTITION BY proposal_acct ORDER BY interv DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING))[1] AS pmb_value
     FROM grouped
   )
   SELECT proposal_acct,
         interv,
-        (fmb_value).price AS fail_market_price,
-        (fmb_value).base_amount AS fail_market_base_amount,
-        (fmb_value).quote_amount AS fail_market_quote_amount,
-        (fmb_value).prices_type AS fail_market_prices_type,
-        (fmb_value).market_acct AS fail_market_acct,
-        (pmb_value).price AS pass_market_price,
-        (pmb_value).base_amount AS pass_market_base_amount,
-        (pmb_value).quote_amount AS pass_market_quote_amount,
-        (pmb_value).prices_type AS pass_market_prices_type,
-        (pmb_value).market_acct AS pass_market_acct
+        (fmb_value).price AS fmb_price,
+        (fmb_value).base_amount AS fmb_base_amount,
+        (fmb_value).quote_amount AS fmb_quote_amount,
+        (fmb_value).prices_type AS fmb_prices_type,
+        (fmb_value).market_acct AS fmb_market_acct,
+        (pmb_value).price AS pmb_price,
+        (pmb_value).base_amount AS pmb_base_amount,
+        (pmb_value).quote_amount AS pmb_quote_amount,
+        (pmb_value).prices_type AS pmb_prices_type,
+        (pmb_value).market_acct AS pmb_market_acct
   FROM ffilled
   WHERE (fmb_value).price IS NOT NULL AND (pmb_value).price IS NOT NULL
   ORDER BY proposal_acct, interv;
