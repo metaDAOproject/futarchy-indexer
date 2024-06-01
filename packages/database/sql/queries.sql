35650253e7dec9d421159e072748e8ce8feb2368
Kollan House <kollanh@gmail.com>
2024-06-01 14:28:25 +0800

    feat: adds new query to fetch chart data

---
+++ packages/database/sql/queries.sql
@@ -0,0 +1,123 @@
+WITH proposal_acct AS ( 
+	SELECT 'GwDTdh3CmxuRjSjvtYAaifdquS1Zwm34W2MiBVAAV4CF' AS proposal_acct
+), proposal_date AS (
+	SELECT
+		proposal_acct,
+		pass_market_acct,
+		fail_market_acct,
+		MIN(created_at)::TIMESTAMPTZ AS created_at,
+		MAX(ended_at)::TIMESTAMPTZ AS ended_at
+	FROM proposals
+	WHERE proposal_acct = (SELECT proposal_acct FROM proposal_acct)
+	GROUP BY proposal_acct, pass_market_acct, fail_market_acct
+	LIMIT 1
+), series AS (
+	SELECT
+		time_series_generated
+	FROM
+		GENERATE_SERIES(
+			TIME_BUCKET(INTERVAL '15 SECONDS', (SELECT created_at FROM proposal_date)),
+			TIME_BUCKET(INTERVAL '15 SECONDS', (SELECT ended_at FROM proposal_date)),
+			INTERVAL '30 SECONDS'
+		) AS time_series_generated
+), matching_pass_data AS (
+	SELECT
+		TIME_BUCKET(INTERVAL '30 SECONDS', created_at) AS created_at,
+		market_acct,
+		LAST(prices.price, price) AS price,
+		base_amount,
+		quote_amount
+	FROM
+		prices
+	WHERE prices.market_acct = (SELECT pass_market_acct FROM proposal_date)
+	AND prices.created_at <= (SELECT ended_at FROM proposal_date)
+	GROUP BY created_at, market_acct, base_amount, quote_amount
+), matching_fail_data AS (
+	SELECT
+		TIME_BUCKET(INTERVAL '30 SECONDS', created_at) AS created_at,
+		market_acct,
+		LAST(prices.price, price) AS price,
+		base_amount,
+		quote_amount
+	FROM
+		prices
+	WHERE prices.market_acct = (SELECT fail_market_acct FROM proposal_date)
+	AND prices.created_at <= (SELECT ended_at FROM proposal_date)
+	GROUP BY created_at, market_acct, base_amount, quote_amount
+), aggregate_pass_data_with_series AS (
+	SELECT
+		created_at,
+		market_acct,
+		price,
+		base_amount,
+		quote_amount
+	FROM matching_pass_data
+	UNION
+	SELECT
+		time_series_generated created_at,
+		NULL market_acct,
+		NULL price,
+		NULL base_amount,
+		NULL quote_amount
+	FROM
+		series
+	ORDER BY created_at DESC
+), aggregate_fail_data_with_series AS (
+	SELECT
+		created_at,
+		market_acct,
+		price,
+		base_amount,
+		quote_amount
+	FROM matching_fail_data
+	UNION
+	SELECT
+		time_series_generated created_at,
+		NULL market_acct,
+		NULL price,
+		NULL base_amount,
+		NULL quote_amount
+	FROM
+		series
+	ORDER BY created_at DESC
+), forward_fill_pass AS (
+	SELECT
+		created_at,
+		market_acct,
+		MAX(price) FILTER (WHERE price IS NOT NULL) OVER lookback AS price,
+		MAX(base_amount) FILTER (WHERE base_amount IS NOT NULL) OVER lookback AS base_amount,
+		MAX(quote_amount) FILTER (WHERE quote_amount IS NOT NULL) OVER lookback AS quote_amount
+	FROM
+		aggregate_pass_data_with_series
+	GROUP BY created_at, market_acct, price, base_amount, quote_amount
+	WINDOW lookback AS (PARTITION BY market_acct, created_at ORDER BY created_at DESC)
+	
+), forward_fill_fail AS (
+	SELECT
+		created_at,
+		market_acct,
+		MAX(price) FILTER (WHERE price IS NOT NULL) OVER lookback AS price,
+		MAX(base_amount) FILTER (WHERE base_amount IS NOT NULL) OVER lookback AS base_amount,
+		MAX(quote_amount) FILTER (WHERE quote_amount IS NOT NULL) OVER lookback AS quote_amount
+	FROM
+		aggregate_fail_data_with_series
+	GROUP BY created_at, market_acct, price, base_amount, quote_amount
+	WINDOW lookback AS (PARTITION BY market_acct, created_at ORDER BY created_at DESC)
+)
+SELECT
+	forward_fill_pass.created_at,
+	(SELECT proposal_acct FROM proposal_acct) AS proposal_acct,
+	forward_fill_pass.market_acct AS pass_market_acct,
+	(forward_fill_pass.price) AS pass_price,
+	(forward_fill_pass.base_amount) AS pass_base_amount,
+	(forward_fill_pass.quote_amount) AS pass_quote_amount,
+	forward_fill_fail.market_acct AS fail_market_acct,
+	forward_fill_fail.price AS fail_price,
+	forward_fill_fail.base_amount AS fail_base_amount,
+	forward_fill_fail.quote_amount AS fail_quote_amount
+FROM forward_fill_pass
+JOIN forward_fill_fail ON forward_fill_pass.created_at = forward_fill_fail.created_at
+WHERE 
+	forward_fill_fail.market_acct IS NOT NULL
+	AND forward_fill_pass.market_acct IS NOT NULL
+ORDER BY forward_fill_pass.created_at DESC;
