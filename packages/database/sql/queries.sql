18bedfe2bbcb983241701a783a6bd4ead2c75320
Kollan House <kollanh@gmail.com>
2024-06-03 23:07:27 +0800

    feat: final working bar generator and table for staging

--- packages/database/sql/queries.sql
+++ packages/database/sql/queries.sql
@@ -12,115 +12,108 @@ RETURNS TABLE (bar_start_time    TIMESTAMPTZ,
 LANGUAGE SQL
 AS
 $$
-WITH proposal_date AS (
+WITH proposal AS (
 	SELECT
-		proposal_acct,
-		pass_market_acct,
-		fail_market_acct,
-		MIN(created_at)::TIMESTAMPTZ AS created_at,
-		MAX(ended_at)::TIMESTAMPTZ AS ended_at
+        pass_market_acct,
+        fail_market_acct,
+        created_at,
+        ended_at
 	FROM proposals
 	WHERE proposal_acct = _proposal_acct
-	GROUP BY proposal_acct, pass_market_acct, fail_market_acct
-	LIMIT 1
 ), series AS (
 	SELECT
-    _proposal_acct AS proposal_acct,
 		time_series_generated
 	FROM
 		GENERATE_SERIES(
-			TIME_BUCKET(_bar_size, (SELECT created_at FROM proposal_date)),
-			TIME_BUCKET(_bar_size, (SELECT ended_at FROM proposal_date)),
+			TIME_BUCKET(_bar_size, (SELECT created_at FROM proposal)),
+			TIME_BUCKET(_bar_size, (SELECT ended_at FROM proposal)),
 			_bar_size
 		) AS time_series_generated
 ), matching_pass_data AS (
 	SELECT
 		TIME_BUCKET(_bar_size, created_at) AS created_at,
-    _proposal_acct AS proposal_acct,
-		market_acct,
 		LAST(prices.price, price) AS price,
-		base_amount,
-		quote_amount
+		LAST(prices.base_amount, base_amount) AS base_amount,
+		LAST(prices.quote_amount, quote_amount) AS quote_amount
 	FROM
 		prices
-	WHERE prices.market_acct = (SELECT pass_market_acct FROM proposal_date)
-	AND prices.created_at <= (SELECT ended_at FROM proposal_date)
-	GROUP BY created_at, market_acct, base_amount, quote_amount
+	WHERE prices.market_acct = (SELECT pass_market_acct FROM proposal)
+		AND prices.created_at <= (SELECT ended_at FROM proposal)
+	GROUP BY created_at
 ), matching_fail_data AS (
 	SELECT
 		TIME_BUCKET(_bar_size, created_at) AS created_at,
-    _proposal_acct AS proposal_acct,
-		market_acct,
 		LAST(prices.price, price) AS price,
-		base_amount,
-		quote_amount
+		LAST(prices.base_amount, base_amount) AS base_amount,
+		LAST(prices.quote_amount, quote_amount) AS quote_amount
 	FROM
 		prices
-	WHERE prices.market_acct = (SELECT fail_market_acct FROM proposal_date)
-	AND prices.created_at <= (SELECT ended_at FROM proposal_date)
-	GROUP BY created_at, market_acct, base_amount, quote_amount
+	WHERE prices.market_acct = (SELECT fail_market_acct FROM proposal)
+		AND prices.created_at <= (SELECT ended_at FROM proposal)
+	GROUP BY created_at
 ), aggregate_pass_data_with_series AS (
 	SELECT
 		time_series_generated AS created_at,
-		series.proposal_acct AS proposal_acct,
-		market_acct,
 		price,
 		base_amount,
 		quote_amount
 	FROM series
 	LEFT JOIN matching_pass_data ON time_series_generated = created_at
-	ORDER BY created_at DESC
 ), aggregate_fail_data_with_series AS (
 	SELECT
 		time_series_generated AS created_at,
-		series.proposal_acct AS proposal_acct,
-		market_acct,
 		price,
 		base_amount,
 		quote_amount
 	FROM series
 	LEFT JOIN matching_fail_data ON time_series_generated = created_at
-	ORDER BY created_at DESC
 ), forward_fill_pass AS (
 	SELECT
-		created_at,
-		proposal_acct,
-		MAX(market_acct) FILTER (WHERE market_acct IS NOT NULL) OVER lookback AS market_acct,
-		MAX(price) FILTER (WHERE price IS NOT NULL) OVER lookback AS price,
-		MAX(base_amount) FILTER (WHERE base_amount IS NOT NULL) OVER lookback AS base_amount,
-		MAX(quote_amount) FILTER (WHERE quote_amount IS NOT NULL) OVER lookback AS quote_amount
-	FROM
-		aggregate_pass_data_with_series
-	GROUP BY created_at, proposal_acct, market_acct, price, base_amount, quote_amount
-	WINDOW lookback AS (PARTITION BY proposal_acct ORDER BY created_at DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+		DISTINCT created_at AS created_at,
+		MIN(price) OVER (PARTITION BY sequence_value) AS price,
+		MIN(base_amount) OVER (PARTITION BY sequence_value) AS base_amount,
+		MIN(quote_amount) OVER (PARTITION BY sequence_value) AS quote_amount
+    FROM
+	(SELECT 
+			created_at,
+			price,
+			base_amount,
+			quote_amount,
+			COUNT(price) OVER (ORDER BY created_at) AS sequence_value
+		FROM aggregate_pass_data_with_series
+	) AS aggregate_pass_data_with_series
 ), forward_fill_fail AS (
 	SELECT
-		created_at,
-		proposal_acct,
-		MAX(market_acct) FILTER (WHERE market_acct IS NOT NULL) OVER lookback AS market_acct,
-		MAX(price) FILTER (WHERE price IS NOT NULL) OVER lookback AS price,
-		MAX(base_amount) FILTER (WHERE base_amount IS NOT NULL) OVER lookback AS base_amount,
-		MAX(quote_amount) FILTER (WHERE quote_amount IS NOT NULL) OVER lookback AS quote_amount
-	FROM
-		aggregate_fail_data_with_series
-	GROUP BY created_at, proposal_acct, market_acct, price, base_amount, quote_amount
-	WINDOW lookback AS (PARTITION BY proposal_acct ORDER BY created_at DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+		DISTINCT created_at AS created_at,
+		MIN(price) OVER (PARTITION BY sequence_value) AS price,
+		MIN(base_amount) OVER (PARTITION BY sequence_value) AS base_amount,
+		MIN(quote_amount) OVER (PARTITION BY sequence_value) AS quote_amount
+    FROM
+	(SELECT 
+			created_at,
+			price,
+			base_amount,
+			quote_amount,
+			COUNT(price) OVER (ORDER BY created_at) AS sequence_value
+		FROM aggregate_fail_data_with_series
+	) AS aggregate_fail_data_with_series
 )
 SELECT
-	forward_fill_pass.created_at,
+	forward_fill_pass.created_at AS created_at,
 	_proposal_acct AS proposal_acct,
-	forward_fill_pass.market_acct AS pass_market_acct,
+	(SELECT pass_market_acct FROM proposal) AS pass_market_acct,
 	(forward_fill_pass.price) AS pass_price,
 	(forward_fill_pass.base_amount) AS pass_base_amount,
 	(forward_fill_pass.quote_amount) AS pass_quote_amount,
-	forward_fill_fail.market_acct AS fail_market_acct,
+	(SELECT fail_market_acct FROM proposal) AS fail_market_acct,
 	forward_fill_fail.price AS fail_price,
 	forward_fill_fail.base_amount AS fail_base_amount,
 	forward_fill_fail.quote_amount AS fail_quote_amount
 FROM forward_fill_pass
 JOIN forward_fill_fail ON forward_fill_pass.created_at = forward_fill_fail.created_at
-ORDER BY forward_fill_pass.created_at DESC;
+ORDER BY created_at DESC;
 $$;
+
 
 CREATE TABLE proposal_bars
 (
@@ -176,7 +169,7 @@ BEGIN
 END;
 $$;
 
-CREATE FUNCTION refresh_all_proposal_bars_action(job_id INT DEFAULT NULL, config JSONB DEFAULT NULL)
+CREATE OR REPLACE FUNCTION refresh_all_proposal_bars_action(job_id INT DEFAULT NULL, config JSONB DEFAULT NULL)
 RETURNS VOID
 LANGUAGE SQL
 AS
