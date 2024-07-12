3ed425c6fcff1e7dc76c6d733aac97cde04df5b7
kilgaloon <radju22@gmail.com>
2024-07-12 16:01:17 +0200

    Update columns to be not null

---
+++ packages/database/drizzle/0055_fixed_captain_midlands.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "user_performance" ALTER COLUMN "tokens_bought" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_performance" ALTER COLUMN "tokens_sold" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_performance" ALTER COLUMN "volume_bought" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "user_performance" ALTER COLUMN "volume_sold" SET NOT NULL;
