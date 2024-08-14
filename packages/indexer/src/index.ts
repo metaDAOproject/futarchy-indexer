ed077a4c1850d99a77f117ac4ae3285e7b775cc3
LukasDeco <ldeconantsesznak@gmail.com>
2024-08-13 22:08:17 -0600

    feat: indexing more fields on daos + proposals

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -3,8 +3,8 @@ import { startIndexerAccountDependencyPopulation } from "./cli/txw/populate";
 import { startTransactionWatchers } from "./transaction/watcher";
 import { startServer } from "./server";
 
-startServer();
-startIndexerAccountDependencyPopulation();
+// startServer();
+// startIndexerAccountDependencyPopulation();
 
-await startTransactionWatchers();
+// await startTransactionWatchers();
 await startIndexers();
