31f85f0e98a22fa6288f1754af58cf7d5006f685
metaproph3t <metaproph3t@protonmail.com>
2024-09-10 00:00:00 +0000

    Get events starting to work

--- packages/indexer/src/indexAmm.ts
+++ packages/indexer/src/indexAmm.ts
@@ -2,8 +2,9 @@ import { Amm, AMM_PROGRAM_ID } from "@metadaoproject/futarchy/v0.4";
 import { usingDb, schema, eq, asc, desc } from "@metadaoproject/indexer-db";
 import { SolanaParser, ParsedInstruction } from "@debridge-finance/solana-transaction-parser";
 import { AmmIDL, AmmClient } from "@metadaoproject/futarchy/v0.4";
-import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { CompiledInnerInstruction, Connection, Keypair, PublicKey } from "@solana/web3.js";
 import { AnchorProvider, Wallet } from "@coral-xyz/anchor";
+import * as anchor from "@coral-xyz/anchor";
 
 const RPC_ENDPOINT = process.env.RPC_ENDPOINT;
 
@@ -13,8 +14,71 @@ if (!RPC_ENDPOINT) {
 
 const connection = new Connection(RPC_ENDPOINT);
 
+const txParser = new SolanaParser([{ idl: AmmIDL, programId: AMM_PROGRAM_ID }]);
+
+  const ammClient = AmmClient.createClient({ provider: new AnchorProvider(connection, new Wallet(Keypair.generate()), {}) });
 
 export const indexAmms = async () => {
+  const signature = "QKiGuARe4hsK4k6CrnGMvYfMPgxQsPbYY5QKVBXSrJt2Pd7QTEcHhxsas8ftAAjuEYFvSnz9MM2jdoqJcYg8YVg";
+
+  // const tx = await txParser.parseTransaction(connection, signature, true);
+  const transactionResponse = await connection.getTransaction(signature, { commitment: "confirmed", maxSupportedTransactionVersion: 1 });
+
+  const events: { name: string; data: any }[] = [];
+  const inner: CompiledInnerInstruction[] =
+    transactionResponse?.meta?.innerInstructions ?? [];
+  const idlProgramId = AMM_PROGRAM_ID;
+  for (let i = 0; i < inner.length; i++) {
+    for (let j = 0; j < inner[i].instructions.length; j++) {
+      const ix = inner[i].instructions[j];
+      const programPubkey =
+        transactionResponse?.transaction.message.staticAccountKeys[
+        ix.programIdIndex
+        ];
+      if (
+        programPubkey === undefined ||
+        !programPubkey.equals(idlProgramId)
+      ) {
+        // we are at instructions that does not match the linked program
+        continue;
+      }
+
+      const ixData = anchor.utils.bytes.bs58.decode(
+        ix.data
+      );
+      const eventData = anchor.utils.bytes.base64.encode(ixData.slice(8));
+      const event = ammClient.program.coder.events.decode(eventData);
+      // const event = program.coder.events.decode(eventData)
+      // console.log(ix.data);
+      console.log(event)
+      if (event) {
+        events.push(event);
+      }
+    }
+  }
+
+  console.log(ammClient.program.coder.events)
+
+  console.log(events[0].data.swapType);
+
+  return;
+  // return events;
+
+  console.log(tx?.meta?.innerInstructions?.[0]);
+  // console.log(ammClient.program.idl.gcc)
+
+  // ammClient.program.coder.
+
+  // const ix = t
+
+
+  // console.log(tx);
+
+  // const log = tx.
+
+  // ammClient.program.coder.events.decode()
+  return;
+
   const eligibleSignatures = await usingDb(async (db) => {
     const tx = await db.select()
       .from(schema.signatures)
@@ -26,7 +90,6 @@ export const indexAmms = async () => {
 
   const highestSequenceNum = eligibleSignatures.reduce((max, signature) => signature.sequence_num > max ? signature.sequence_num : max, 0n);
 
-  const txParser = new SolanaParser([{ idl: AmmIDL, programId: AMM_PROGRAM_ID }]);
 
   const txs = await Promise.all(eligibleSignatures.map(async (signature) => {
     const parsedTx = await txParser.parseTransaction(connection, signature.signature);
@@ -56,7 +119,6 @@ export const indexAmms = async () => {
     }
   });
 
-  const ammClient = AmmClient.createClient({ provider: new AnchorProvider(connection, new Wallet(Keypair.generate()), {}) });
 
   ammsToUpdateOrInsert.forEach(async amm => {
     const chainAmm = await ammClient.fetchAmm(amm) as Amm;
@@ -73,14 +135,14 @@ export const indexAmms = async () => {
           quote_reserves: chainAmm.quoteAmount.toNumber(),
           latest_seq_num_applied: highestSequenceNum,
         }])
-        .onConflictDoUpdate({
-          target: schema.v0_4_amm.amm_addr,
-          set: {
-            base_reserves: chainAmm.baseAmount.toNumber(),
-            quote_reserves: chainAmm.quoteAmount.toNumber(),
-            latest_seq_num_applied: highestSequenceNum,
-          }
-        })
+          .onConflictDoUpdate({
+            target: schema.v0_4_amm.amm_addr,
+            set: {
+              base_reserves: chainAmm.baseAmount.toNumber(),
+              quote_reserves: chainAmm.quoteAmount.toNumber(),
+              latest_seq_num_applied: highestSequenceNum,
+            }
+          })
       });
     }
   });
