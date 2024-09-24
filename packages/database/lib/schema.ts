a695373675bc553ded65476157912463af15b04d
metaproph3t <metaproph3t@protonmail.com>
2024-09-24 00:00:00 -0700

    Add schema changes for v0.4

--- packages/database/lib/schema.ts
+++ packages/database/lib/schema.ts
@@ -17,6 +17,7 @@ import {
   uuid,
   pgView,
   QueryBuilder,
+  bigserial,
 } from "drizzle-orm/pg-core";
 
 // Implementation discussed here https://github.com/metaDAOproject/futarchy-indexer/pull/1
@@ -63,6 +64,11 @@ export enum Reactions {
   LaughingFace = "LaughingFace",
   FrownyFace = "FrownyFace",
   Celebrate = "Celebrate",
+}
+
+export enum V04SwapType {
+  Buy = "Buy",
+  Sell = "Sell",
 }
 
 type NonEmptyList<E> = [E, ...E[]];
@@ -874,6 +880,118 @@ export const userPerformance = pgTable(
     return {
       pk: primaryKey({ columns: [table.proposalAcct, table.userAcct] }),
     };
+  }
+);
+
+export const signatures = pgTable(
+  "signatures",
+  {
+    signature: transaction("signature").notNull(),
+    queriedAddr: pubkey("queried_addr").notNull(),
+    slot: slot("slot").notNull(),
+    didErr: boolean("did_err").notNull(),
+    err: text("err"),
+    blockTime: timestamp("block_time", { withTimezone: true }),
+    insertedAt: timestamp("inserted_at", { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+    seqNum: bigserial('seq_num', { mode: 'bigint' }).notNull().unique(),
+  },
+  (table) => ({
+    pk: primaryKey(table.signature, table.queriedAddr ),
+    slotIdx: index("slot_index").on(table.slot),
+    sequenceNumIdx: index("sequence_num_index").on(table.seqNum),
+    queriedAddrIdx: index("queried_addr_index").on(table.queriedAddr),
+  })
+)
+
+export const v0_4_amms = pgTable(
+  "v0_4_amms",
+  {
+    ammAddr: pubkey("amm_addr").primaryKey(),
+    createdAtSlot: slot("created_at_slot").notNull(),
+    lpMintAddr: pubkey("lp_mint_addr").notNull().references(() => tokens.mintAcct),
+    baseMintAddr: pubkey("base_mint_addr").notNull().references(() => tokens.mintAcct),
+    quoteMintAddr: pubkey("quote_mint_addr").notNull().references(() => tokens.mintAcct),
+    baseReserves: bigint("base_reserves", { mode: "bigint" }).notNull(),
+    quoteReserves: bigint("quote_reserves", { mode: "bigint" }).notNull(),
+    latestAmmSeqNumApplied: bigint("latest_amm_seq_num_applied", { mode: "bigint" }).notNull(),
+    insertedAt: timestamp("inserted_at", { withTimezone: true })
+      .notNull()
+      .default(sql`now()`),
+  }
+)
+
+export const v0_4_metric_decisions = pgTable(
+  "v0_4_metric_decisions",
+  {
+    id: bigserial("id", { mode: "bigint" }).primaryKey(),
+    daoId: bigint("dao_id", { mode: "bigint" }).references(
+      () => daoDetails.daoId
+    ).notNull(),
+    title: text("title").notNull(),
+    description: text("description").notNull(),
+    outcomeQuestionAddr: pubkey("outcome_question_addr").notNull().references(() => v0_4_questions.questionAddr),
+    metricQuestionAddr: pubkey("metric_question_addr").notNull().references(() => v0_4_questions.questionAddr),
+    outcomeVaultAddr: pubkey("outcome_vault_addr").notNull().references(() => v0_4_conditional_vaults.conditionalVaultAddr),
+    metricVaultAddr: pubkey("metric_vault_addr").notNull().references(() => v0_4_conditional_vaults.conditionalVaultAddr),
+    ammAddr: pubkey("amm_addr").notNull().references(() => v0_4_amms.ammAddr),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .notNull()
+      .default(sql`now()`),
+  }
+)
+
+// TODO rename `created_at` to `inserted_at`
+
+export const v0_4_swaps = pgTable(
+  "v0_4_swaps",
+  {
+    signature: transaction("signature").notNull().primaryKey(),
+    slot: slot("slot").notNull(),
+    block_time: timestamp("block_time", { withTimezone: true }).notNull(),
+    swapType: pgEnum("swap_type", V04SwapType).notNull(),
+    ammAddr: pubkey("amm_addr").notNull(),
+    userAddr: pubkey("user_addr").notNull(),
+    inputAmount: tokenAmount("input_amount").notNull(),
+    outputAmount: tokenAmount("output_amount").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .notNull()
+      .default(sql`now()`),
+  },
+)
+
+export const v0_4_questions = pgTable(
+  "v0_4_questions",
+  {
+    questionAddr: pubkey("question_addr").primaryKey(),
+    isResolved: boolean("is_resolved").notNull(),
+    oracleAddr: pubkey("oracle_addr").notNull(),
+    numOutcomes: smallint("num_outcomes").notNull(),
+    payoutNumerators: jsonb("payout_numerators").notNull(),
+    payoutDenominator: bigint("payout_denominator", { mode: "bigint" }).notNull(),
+    questionId: jsonb("question_id").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .notNull()
+      .default(sql`now()`),
+  }
+)
+
+export const v0_4_conditional_vaults = pgTable(
+  "v0_4_conditional_vaults",
+  {
+    conditionalVaultAddr: pubkey("conditional_vault_addr").primaryKey(),
+    questionAddr: pubkey("question_addr").references(() => v0_4_questions.questionAddr).notNull()
+      .references(() => v0_4_questions.questionAddr),
+    underlyingMintAcct: pubkey("underlying_mint_acct").notNull()
+      .references(() => tokens.mintAcct),
+    underlyingTokenAcct: pubkey("underlying_token_acct").notNull()
+      .references(() => tokenAccts.tokenAcct),
+    pdaBump: smallint("pda_bump").notNull(),
+    latestVaultSeqNumApplied: bigint("latest_vault_seq_num_applied", { mode: "bigint" }).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .notNull()
+      .default(sql`now()`),
   }
 );
 
