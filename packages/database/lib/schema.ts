91e9937697dbc40a102d4a1529338229f5635ca9
metaproph3t <metaproph3t@protonmail.com>
2024-10-02 00:00:00 -0700

    Add `v0_4_splits` table

--- packages/database/lib/schema.ts
+++ packages/database/lib/schema.ts
@@ -991,7 +991,28 @@ export const v0_4_swaps = pgTable("v0_4_swaps", {
 }, (table) => ({
     ammIdx: index("amm_index").on(table.ammAddr),
     signatureIdx: index("signature_index").on(table.signature),
-    slotAmmIdx: index("seq_num_amm_index").on(table.ammSeqNum, table.ammAddr),
+    seqNumAmmIdx: index("seq_num_amm_index").on(table.ammSeqNum, table.ammAddr),
+  })
+);
+
+export const v0_4_splits = pgTable(
+  "v0_4_splits",
+  {
+    id: bigserial("id", { mode: "bigint" }).primaryKey(),
+    vaultAddr: pubkey("vault_addr").notNull().references(() => v0_4_conditional_vaults.conditionalVaultAddr),
+    vaultSeqNum: bigint("vault_seq_num", { mode: "bigint" }),
+    signature: transaction("signature").notNull().references(() => signatures.signature),
+    slot: slot("slot").notNull().references(() => signatures.slot),
+    amount: bigint("amount", { mode: "bigint" }).notNull(),
+    questionAddr: pubkey("question_addr").references(() => v0_4_questions.questionAddr).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .notNull()
+      .default(sql`now()`),
+  },
+  (table) => ({
+    vaultIdx: index("vault_index").on(table.vaultAddr),
+    signatureIdx: index("signature_index").on(table.signature),
+    seqNumVaultIdx: index("seq_num_vault_index").on(table.vaultSeqNum, table.vaultAddr),
   })
 );
 
