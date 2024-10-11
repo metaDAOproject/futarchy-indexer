37fdf5c3cb1ff7e67d7e0ce8d1057b2ce5bff01d
LukasDeco <ldeconantsesznak@gmail.com>
2024-10-11 10:18:52 -0600

    refactor: cleanup migrations for v4

--- packages/database/drizzle/0002_dizzy_titanium_man.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "v0_4_metric_decisions" ADD COLUMN "recipient" text DEFAULT '' NOT NULL;--> statement-breakpoint
-ALTER TABLE "v0_4_metric_decisions" ADD COLUMN "market_opened" timestamp with time zone DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "v0_4_metric_decisions" ADD COLUMN "grant_awarded" timestamp with time zone DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "v0_4_metric_decisions" ADD COLUMN "committee_evaluation" timestamp with time zone DEFAULT now() NOT NULL;
